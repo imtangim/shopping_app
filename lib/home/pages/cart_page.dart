1039ee9ab6ec221264866f3e40c8b1b80810dbb1
Tangim <tangim15-4103@diu.edu.bd>
2023-07-18 01:59:33 +0600

    feature update

--- lib/home/pages/cart_page.dart
+++ lib/home/pages/cart_page.dart
@@ -10,82 +10,98 @@ class CartPage extends StatelessWidget {
     // final cart = Provider.of<CartProvider>(context).cart;
     final cart = context.watch<CartProvider>().cart;
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Cart"),
-      ),
-      body: ListView.builder(
-        itemCount: cart.length,
-        itemBuilder: (context, index) {
-          final cartitem = cart[index];
+    if (cart.isNotEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Cart"),
+        ),
+        body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index) {
+            final cartitem = cart[index];
 
-          return ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundImage: AssetImage(
-                cartitem['imageUrl'].toString(),
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage(
+                  cartitem['imageUrl'].toString(),
+                ),
               ),
-            ),
-            title: Text(
-              cartitem['title'].toString(),
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            subtitle: Text("Size: ${cartitem['size']}"),
-            trailing: IconButton(
-                onPressed: () {
-                  showDialog(
-                    barrierDismissible: false,
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text(
-                          "Delete Product",
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        content: Text(
-                          "Are you sure?",
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        actions: [
-                          TextButton(
+              title: Text(
+                cartitem['title'].toString(),
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              subtitle: Text("Size: ${cartitem['size']}"),
+              trailing: IconButton(
+                  onPressed: () {
+                    showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text(
+                            "Delete Product",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          content: Text(
+                            "Are you sure?",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  "No",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                            TextButton(
                               onPressed: () {
+                                context
+                                    .read<CartProvider>()
+                                    .removeProduct(cartitem);
+                                // Provider.of<CartProvider>(context, listen: false)
+                                //     .removeProduct(cartitem);
                                 Navigator.of(context).pop();
                               },
                               child: const Text(
-                                "No",
+                                "Yes",
                                 style: TextStyle(
-                                  color: Colors.blue,
+                                  color: Colors.red,
                                   fontWeight: FontWeight.bold,
                                 ),
-                              )),
-                          TextButton(
-                            onPressed: () {
-                              context.read<CartProvider>().removeProduct(cartitem);
-                              // Provider.of<CartProvider>(context, listen: false)
-                              //     .removeProduct(cartitem);
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text(
-                              "Yes",
-                              style: TextStyle(
-                                color: Colors.red,
-                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                )),
-          );
-        },
-      ),
-    );
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  )),
+            );
+          },
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: Text(
+            "Cart is empty",
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 25,
+                ),
+          ),
+        ),
+      );
+    }
   }
 }
